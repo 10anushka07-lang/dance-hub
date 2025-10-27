82b7d6a7bbe73f27edfdbbeaff53dd3f94182ab7
anushka <10anushka07@gmail.com>
2025-10-27 10:01:07 +0530

    added new files

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const links = document.querySelectorAll('nav ul li a');
+
+for (const link of links) {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+}
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Thank you! Your message has been sent.');
+    form.reset();
+});
